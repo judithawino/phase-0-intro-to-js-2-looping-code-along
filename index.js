9cd8990c9038668d0601d4410fe27c342be14058
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-06-04 06:16:51 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+
+ // 1) Create a new, empty array to hold the messages.
+ const messages = [];
+ //2) Iterate through the input array
+  for (let count = 0; count < names.length; count++) {
+
+// 3) using string interpolation inside the loop, build out the thank you message. 
+    const message = `Thank you, ${names[count]}, for the wonderful ${eventName} gift!`;
+
+// 4) Add the message to the new empty message array we created earlier.
+    messages.push(message);
+  }
+// 3) return the new array once the loop comes to an end.
+  
+  return messages;
+
+}
+
+console.log(writeCards([], ''));
+
+function countDown(){
+    let countDown = 10;
+    while (countDown){
+        
+        console.log(countDown--);
+    }
+console.log(countDown);
+}
+console.log(countDown());
+   
